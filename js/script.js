3baffbef251a31a4dbd54bb0f31c72ab762ba0fb
shelllrz <michellealejanddrina45@gmail.com>
2026-05-19 22:52:56 -0300

    Primeira versão AgroNexus

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function sincronizar(){
+
+    alert("Dados sincronizados com sucesso!");
+
+}
+
+function falar(){
+
+    document.getElementById("textoIA").innerText =
+    "Comando recebido: verificando dados da produção.";
+
+}
